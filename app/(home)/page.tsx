0c3fab6f6b0bf03a15a7f8c0486368769ac723ab
Arison Firmino <arisonfirmino.dev@gmail.com>
2024-02-29 13:44:20 -0300

    feat: add initial CityDateTime component

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,9 +1,14 @@
+import CityDateTime from "../components/city-date-time";
 import Header from "../components/header";
 
 export default function Home() {
   return (
     <main className="flex flex-col gap-10 p-5">
       <Header />
+
+      <section>
+        <CityDateTime />
+      </section>
     </main>
   );
 }
