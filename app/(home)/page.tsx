1ab1619442ca2c743c63a3ea66e5891b44668555
Arison Firmino <arisonfirmino.dev@gmail.com>
2024-02-29 22:47:34 -0300

    feat: add location function using geolocation

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useEffect, useState, useCallback } from "react";
 import Header from "../components/header";
 import CityDate from "../components/city-date";
 import Search from "../components/search";
@@ -9,26 +9,64 @@ import { AlertCircleIcon } from "lucide-react";
 
 export default function Home() {
   const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
-  const [cityName, setCityName] = useState("Londres");
-  const API_URL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}&units=metric`;
-
+  const [cityName, setCityName] = useState("");
   const [weatherData, setWeatherData] = useState(null);
   const [notFound, setNotFound] = useState(false);
 
-  const handleSearch = (city: string) => {
+  const fetchWeatherData = useCallback(
+    async (city: string) => {
+      const response = await fetch(
+        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${API_KEY}&units=metric`,
+      );
+
+      if (!response.ok) {
+        throw new Error("Failed to fetch data");
+      }
+
+      const data = await response.json();
+      setWeatherData(data);
+      setNotFound(false);
+    },
+    [API_KEY],
+  );
+
+  const handleSearch = async (city: string) => {
     setCityName(city);
+
+    try {
+      await fetchWeatherData(city);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+      setWeatherData(null);
+      setNotFound(true);
+    }
   };
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch(API_URL);
-        if (!response.ok) {
-          throw new Error("Failed to fetch data");
+        let location = "Londres";
+
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition((position) => {
+            const { latitude, longitude } = position.coords;
+
+            fetch(
+              `https://api.openweathermap.org/geo/1.0/reverse?lat=${latitude}&lon=${longitude}&limit=1&appid=${API_KEY}`,
+            )
+              .then((response) => response.json())
+              .then((data) => {
+                if (data && data[0] && data[0].name) {
+                  setCityName(data[0].name);
+
+                  fetchWeatherData(data[0].name);
+                }
+              })
+              .catch((error) =>
+                console.error("Error fetching location:", error),
+              );
+          });
         }
-        const data = await response.json();
-        setWeatherData(data);
-        setNotFound(false);
       } catch (error) {
         console.error("Error fetching data:", error);
         setWeatherData(null);
@@ -36,7 +74,7 @@ export default function Home() {
       }
     };
     fetchData();
-  }, [API_URL]);
+  }, [API_KEY, fetchWeatherData]);
 
   return (
     <main className="flex min-h-screen w-full flex-col gap-10 p-5">
