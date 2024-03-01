1ef9daef9cc3a4c430a3170dd6265ef342bedd7e
Arison Firmino <arisonfirmino.dev@gmail.com>
2024-02-29 22:29:01 -0300

    feat: add a conditional check to display a message when weather data is not found and hides the CityDate component

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -5,6 +5,7 @@ import Header from "../components/header";
 import CityDate from "../components/city-date";
 import Search from "../components/search";
 import TemperatureDisplay from "../components/temperature-display";
+import { AlertCircleIcon } from "lucide-react";
 
 export default function Home() {
   const API_KEY = process.env.NEXT_PUBLIC_API_KEY;
@@ -12,21 +13,29 @@ export default function Home() {
   const API_URL = `https://api.openweathermap.org/data/2.5/weather?q=${cityName}&appid=${API_KEY}&units=metric`;
 
   const [weatherData, setWeatherData] = useState(null);
+  const [notFound, setNotFound] = useState(false);
 
   const handleSearch = (city: string) => {
     setCityName(city);
   };
 
   useEffect(() => {
-    fetch(API_URL)
-      .then((response) => response.json())
-      .then((data: any) => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch(API_URL);
+        if (!response.ok) {
+          throw new Error("Failed to fetch data");
+        }
+        const data = await response.json();
         setWeatherData(data);
-        console.log(data);
-      })
-      .catch((error) => {
+        setNotFound(false);
+      } catch (error) {
         console.error("Error fetching data:", error);
-      });
+        setWeatherData(null);
+        setNotFound(true);
+      }
+    };
+    fetchData();
   }, [API_URL]);
 
   return (
@@ -34,7 +43,14 @@ export default function Home() {
       <Header />
 
       <section className="flex w-full flex-col items-center gap-5 xl:flex-row xl:justify-between xl:gap-0">
-        <CityDate weatherData={weatherData} />
+        {notFound && (
+          <p className="flex items-center gap-2 text-xl text-red-600">
+            <AlertCircleIcon size={20} />
+            Não Encontrado
+          </p>
+        )}
+
+        {!notFound && weatherData && <CityDate weatherData={weatherData} />}
         <Search onSearch={handleSearch} />
       </section>
 
--- app/components/city-date.tsx
+++ app/components/city-date.tsx
@@ -18,9 +18,9 @@ export default function CityDate({ weatherData }: any) {
   } de ${currentDate.getFullYear()}`;
 
   return (
-    <div className="flex cursor-default flex-col items-center gap-1">
+    <div className="flex cursor-default flex-col items-center gap-1 xl:items-start">
       <h1 className="text-3xl font-medium">
-        {weatherData?.name}, {weatherData?.sys.country}
+        {weatherData?.name}, {weatherData?.sys?.country}
       </h1>
       <p className="tet-xl">{formattedDate}</p>
     </div>
